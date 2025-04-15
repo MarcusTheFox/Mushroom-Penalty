27f6d4d7675be215c61b09b42b1d440e86cb9647
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-04-15 17:49:37 +0300

    Misha: Feat(sounds, bootstrap): Add sound manager and main scene bootstrap

---
+++ Assets/Scripts/Bootstraps/GameProcessSceneBootstrap.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameProcessSceneBootstrap : MonoBehaviour
+{
+    void Start()
+    {
+        PlayLevelMusic();
+    }
+    
+    void PlayLevelMusic()
+    {
+        SoundManager.Instance.PlayMusic("Birds");
+    }
+}
